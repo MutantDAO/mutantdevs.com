80bdcc05919eb12acdf6af49ccdd8d715e717b8f
Kath Odler <kathodler@protonmail.com>
2021-11-29 15:28:16 +1000

    Add Registration Closes

--- components/KeyDates.js
+++ components/KeyDates.js
@@ -6,7 +6,7 @@ import styles from "./KeyDates.module.css";
 const GuidelinesSection = () => {
   const dates = [
     { date: "Nov 29", desc: "Registrations open" },
-    { date: "Dec 3", desc: "Hacking begins" },
+    { date: "Dec 3", desc: "Registrations close & Hacking begins!" },
     { date: "Dec 13", desc: "Submissions close" },
     { date: "Dec 16", desc: "Validation / security testing begins" },
     { date: "Dec 17", desc: "Projects are deployed to Mainnet" },
