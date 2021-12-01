26cb08f7079c3c61f89343527e8150c11aa1ad55
Kath Odler <kathodler@protonmail.com>
2021-12-01 12:46:12 +1000

    use next link to prevent reload of whole page

--- components/MutantNav.js
+++ components/MutantNav.js
@@ -1,7 +1,7 @@
 import { Navbar, Container, Col, Row, Button } from "react-bootstrap";
 import RegisterBtn from "./RegisterBtn";
 import styles from "./MutantNav.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 const MutantNav = (props) => {
   // black;
@@ -12,30 +12,35 @@ const MutantNav = (props) => {
       variant="dark"
     >
       <Container>
-        <Navbar.Brand href="/" className={styles.brandContainer}>
-          <img
-            alt="Mutant Cats Logo"
-            src="/Logo_2A_1024x1024.png"
-            width="30"
-            height="30"
-            className={`${styles.brandIcon} d-inline-block align-top`}
-          />
-          <span style={{ marginLeft: "0px" }}>Mutant Devs</span>
-        </Navbar.Brand>
+        <Link href="/">
+          <Navbar.Brand className={styles.brandContainer}>
+            <img
+              alt="Mutant Cats Logo"
+              src="/Logo_2A_1024x1024.png"
+              width="30"
+              height="30"
+              className={`${styles.brandIcon} d-inline-block align-top`}
+            />
+            <span style={{ marginLeft: "0px" }}>Mutant Devs</span>
+          </Navbar.Brand>
+        </Link>
         <Col>
           <Link href="/resources" passHref>
-            <Button variant="dark" className={styles.customBtn} >Resources</Button>
+            <Button variant="dark" className={styles.customBtn}>
+              Resources
+            </Button>
           </Link>
         </Col>
 
-
         <Row>
           <Col>
-            <RegisterBtn analytics={props.analytics} origin={"navbar"}>Register</RegisterBtn>
+            <RegisterBtn analytics={props.analytics} origin={"navbar"}>
+              Register
+            </RegisterBtn>
           </Col>
         </Row>
-      </Container >
-    </Navbar >
+      </Container>
+    </Navbar>
   );
 };
 
